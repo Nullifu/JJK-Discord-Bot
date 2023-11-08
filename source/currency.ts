02f8308af281289de590c4d802cd3ca7e987847a
viral32111 <19510403+viral32111@users.noreply.github.com>
2023-11-08 19:48:42 +0000

    Fix your code again

--- source/currency.ts
+++ source/currency.ts
@@ -1,38 +1,36 @@
-import db from './currencydb.js';
-
-
+import db from "./currencydb.js"
 
 // Function to get a user's currency
 export async function getUserCurrency(userId: string): Promise<number> {
-  await db.read();
-  const record = db.data?.records.find(rec => rec.id === userId);
-  return record?.currency ?? 0;
+	await db.read()
+	const record = db.data?.records.find(rec => rec.id === userId)
+	return record?.currency ?? 0
 }
 
 // Function to add currency to a user
 export async function addUserCurrency(userId: string, amount: number): Promise<void> {
-  await db.read();
-  const record = db.data?.records.find(rec => rec.id === userId);
+	await db.read()
+	const record = db.data?.records.find(rec => rec.id === userId)
 
-  if (record) {
-    record.currency += amount;
-  } else {
-    db.data?.records.push({ id: userId, currency: amount });
-  }
+	if (record) {
+		record.currency += amount
+	} else {
+		db.data?.records.push({ id: userId, currency: amount })
+	}
 
-  await db.write();
+	await db.write()
 }
 
 // Function to reduce a user's currency
 export async function removeUserCurrency(userId: string, amount: number): Promise<void> {
-  await db.read();
-  const record = db.data?.records.find(rec => rec.id === userId);
+	await db.read()
+	const record = db.data?.records.find(rec => rec.id === userId)
 
-  if (record && record.currency >= amount) {
-    record.currency -= amount;
-  } else {
-    throw new Error("User doesn't have enough currency.");
-  }
+	if (record && record.currency >= amount) {
+		record.currency -= amount
+	} else {
+		throw new Error("User doesn't have enough currency.")
+	}
 
-  await db.write();
+	await db.write()
 }
--- source/db.ts
+++ source/db.ts
@@ -1,77 +1,75 @@
-import { JSONFile,  } from 'lowdb/node';
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { Low } from 'lowdb';
-import { LowSync } from 'lowdb';
+import { Low } from "lowdb"
+import { JSONFile } from "lowdb/node"
+import { dirname, join } from "path"
+import { fileURLToPath } from "url"
 
 // If you're using ES modules, for example in a TypeScript setting
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // Define your types and interfaces
 type User = {
-  currency: number;
-  inventory: Record<string, InventoryItem>; // Using a record to keep track of item quantities
-  job?: string; // Optional job field if a user has a job
-};
+	currency: number
+	inventory: Record<string, InventoryItem> // Using a record to keep track of item quantities
+	job?: string // Optional job field if a user has a job
+}
 
 // Define what an inventory item looks like
 interface InventoryItem {
-  itemId: string;
-  quantity: number;
+	itemId: string
+	quantity: number
 }
 
 interface Item {
-  id: string;
-  name: string;
-  value: number; // Value could be used for buying/selling
+	id: string
+	name: string
+	value: number // Value could be used for buying/selling
 }
 
 interface Job {
-  name: string;
-  cost: number;
-  payout: { min: number; max: number };
+	name: string
+	cost: number
+	payout: { min: number; max: number }
 }
 
 interface Data {
-  users: Record<string, User>;
-  items: Record<string, Item>; // A record of all available items by id
-  jobs: Job[];
+	users: Record<string, User>
+	items: Record<string, Item> // A record of all available items by id
+	jobs: Job[]
 }
 
-
-
 // Set up a JSON file for storage
-const file = join(__dirname, 'db.json');
-const adapter = new JSONFile<Data>(file);
+const file = join(__dirname, "db.json")
+const adapter = new JSONFile<Data>(file)
 
 // Define a default structure for your database
 const defaultData: Data = {
-  users: {},
-  items: { // Adding a few default items
-    'sword': { id: 'sword', name: 'Sword', value: 100 },
-    'shield': { id: 'shield', name: 'Shield', value: 150 },
-    // ...other items
-  },
-  jobs: [
-    // You can define default jobs here
-    { name: 'Cashier', cost: 100, payout: { min: 20, max: 30 } },
-    { name: 'Security Guard', cost: 200, payout: { min: 30, max: 40 } },
-    // ...other default jobs
-  ],
-};
+	users: {},
+	items: {
+		// Adding a few default items
+		sword: { id: "sword", name: "Sword", value: 100 },
+		shield: { id: "shield", name: "Shield", value: 150 }
+		// ...other items
+	},
+	jobs: [
+		// You can define default jobs here
+		{ name: "Cashier", cost: 100, payout: { min: 20, max: 30 } },
+		{ name: "Security Guard", cost: 200, payout: { min: 30, max: 40 } }
+		// ...other default jobs
+	]
+}
 
 // Create an instance of LowDB with the JSON file and the default data
-const db = new Low<Data>(adapter, defaultData);
+const db = new Low<Data>(adapter, defaultData)
 
 // Initialize the database with default data
 const initializeDB = async () => {
-  await db.read();
-  db.data ||= defaultData;
-  if (db.data.jobs.length === 0) {
-    db.data.jobs = defaultData.jobs;
-  }
-  await db.write();
-};
+	await db.read()
+	db.data ||= defaultData
+	if (db.data.jobs.length === 0) {
+		db.data.jobs = defaultData.jobs
+	}
+	await db.write()
+}
 
-export default db;
-export { defaultData, db, initializeDB, User, Item, Job, Data };
+export default db
+export { Data, Item, Job, User, db, defaultData, initializeDB }
