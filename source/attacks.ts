4e9ac9945f71968e80e808b54f63ea45790ab951
nullifu <ayanazuipix@outlook.com>
2024-04-27 16:41:48 +0100

    fixes

--- source/attacks.ts
+++ source/attacks.ts
@@ -7,15 +7,15 @@ interface Attack {
 function getGradeDamageBonus(grade: string): number {
 	switch (grade) {
 		case "Special Grade":
-			return 2.0
+			return 1.5
 		case "Grade 1":
-			return 1.7
+			return 1.3
 		case "Semi-Grade 1":
-			return 1.4
-		case "Grade 2":
-			return 1.2
-		case "Grade 3":
 			return 1.1
+		case "Grade 2":
+			return 1.0
+		case "Grade 3":
+			return 1.0
 		case "Grade 4":
 		default:
 			return 1.0
